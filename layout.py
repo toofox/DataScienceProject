1d652af6dfc8c6a9195570d3a85877a178269eaf
Louis Krückmeyer <louis.krueckmeyer@gmx.de>
2024-09-10 17:13:59 +0200

    add the new libary for chi square test

--- layout.py
+++ layout.py
@@ -7,6 +7,7 @@ import numpy as np
 from collections import Counter
 import re
 import dash
+from scipy.stats import chi2_contingency
 
 #chatgpt:
 # Um  eigene CSV-Datei einzufügen, ersetzen Sie 'your_file.csv' durch den Pfad zu Ihrer CSV-Datei.
